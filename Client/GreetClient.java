d1b24cd31814f6d0cdc61ae7d7a0281f520951f6
Pamela S-S <atheanae@gmail.com>
2022-02-17 12:13:35 -0500

    Socket - add client and server files

---
+++ Client/GreetClient.java
@@ -0,0 +1,87 @@
+/* implementation of a client-server application using sockets.
+The system consists of a client and a server to implement a Simple Date-Time Info Protocol 
+@Author PamelaESS
+*/
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+import java.util.Objects;
+
+public class GreetClient {
+   public static void main(String[] args) throws Exception {
+      String serverName = args[0];
+      int port = Integer.parseInt(args[1]);
+
+      Scanner input = new Scanner(System.in);
+      System.out.print("CLIENT: ");
+      String firstInput = "";
+
+      while (!Objects.equals(firstInput, "EXIT")) {
+         firstInput = input.nextLine();
+         if (Objects.equals(firstInput, "HELP")) {
+            System.out.println("*** These are the commands that you can use:-"
+                  + "\nHELLO: required for starting the STDP session."
+                  + "\nHELP:  displays the help information about the protocol. It may be received any time whether and STDP session is established or not."
+                  + "\nDOW: returns the day of week" + "\nTIME: returns the current time."
+                  + "\nDATE: returns the current date." + "\nDATETIME: returns the current date and time."
+                  + "\nBYE: ends the session.");
+            System.out.print("CLIENT: ");
+         } else if (Objects.equals(firstInput, "HELLO")) {
+            break;
+         } else {
+            System.out.print("CLIENT: ");
+         }
+      }
+
+      if (Objects.equals(firstInput, "HELLO")) {
+
+         try {
+            System.out.println("Connecting to " + serverName + " on port " + port);
+            Socket client = new Socket(serverName, port);
+
+            System.out.println("Just connected to " + client.getRemoteSocketAddress() + "\n");
+
+            OutputStream outToServer = client.getOutputStream();
+            DataOutputStream out = new DataOutputStream(outToServer);
+
+            out.writeUTF("Hello from " + client.getLocalSocketAddress());
+            InputStream inFromServer = client.getInputStream();
+            DataInputStream in = new DataInputStream(inFromServer);
+
+            System.out.println(in.readUTF());
+            System.out.print("CLIENT: ");
+
+            // string to read message from input
+            String line = "";
+            String inBound = "";
+            String onBye = ((Object) client.getRemoteSocketAddress()).toString();
+            int index = onBye.indexOf("/");
+            String address = onBye.substring(index, onBye.length());
+
+            while (!line.equals("BYE") && !line.equals("EXIT")) {
+               try {
+                  line = input.nextLine();
+                  out.writeUTF(line);
+                  inBound = in.readUTF();
+                  System.out.println("SERVER: " + inBound);
+                  if ((Objects.equals(inBound, "Input Invalid - Closing connection. \n"))
+                        || (Objects.equals(inBound, "Thank you for connecting to " + address + "\nGoodbye!"))) {
+
+                     break;
+                  }
+                  System.out.print("CLIENT: ");
+               } catch (IOException i) {
+                  // System.out.println(i);
+               }
+            }
+
+            client.close();
+            input.close();
+         } catch (IOException e) {
+            // System.out.println("Exception");
+            // e.printStackTrace();
+         }
+      }
+   }
+}
---
+++ Server/GreetServer.java
@@ -0,0 +1,136 @@
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.Objects;
+import java.time.LocalDate;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.concurrent.Executors;
+
+public class GreetServer {
+
+   /**
+    * Runs the server. When a client connects, the server creates a new thread The
+    * maximum number of threads via a thread pool set to 20 (otherwise there can be
+    * a clients requests overload that could cause the server to run out of
+    * resources by allocating too many threads).
+    */
+   public static void main(String[] args) throws Exception {
+
+      int port = Integer.parseInt(args[0]);
+      try {
+         ServerSocket listener = new ServerSocket(port);
+
+         System.out.println("Server is running...");
+         var pool = Executors.newFixedThreadPool(20);
+         while (true) {
+            pool.execute(new GreetingServer(listener.accept()));
+         }
+
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+   }
+
+   private static class GreetingServer implements Runnable {
+      private Socket socket;
+
+      GreetingServer(Socket socket) {
+         this.socket = socket;
+      }
+
+      public void run() {
+         try {
+
+            System.out.println("Waiting for client on port " + socket.getLocalPort() + "...");
+
+            System.out.println("Connected: " + socket.getRemoteSocketAddress());
+
+            DataInputStream in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+
+            System.out.println("CLIENT : " + in.readUTF());
+
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+            out.writeUTF("Hello CCCS431 SDTP Server written by ... " + "READY" + "\n\n" + "Hit enter to stop. \n");
+
+            String day = LocalDate.now().getDayOfWeek().name();
+            Date time = new java.util.Date(System.currentTimeMillis());
+            Date dateTime = new java.util.Date(System.currentTimeMillis());
+            String help = "*** These are the commands that you can use:-"
+                  + "\nHELLO: required for starting the STDP session."
+                  + "\nHELP:  displays the help information about the protocol. It may be received any time whether and STDP session is established or not."
+                  + "\nDOW: returns the day of week" + "\nTIME: returns the current time."
+                  + "\nDATE: returns the current date." + "\nDATETIME: returns the current date and time."
+                  + "\nBYE: ends the session. \n";
+
+            String line = "";
+
+            // keep reading until "Over" is input
+            while (!line.equals("BYE") && !line.equals("EXIT")) {
+               try {
+
+                  line = in.readUTF();
+                  System.out.println(line.toUpperCase());
+
+                  if (Objects.equals(line, "HELP")) {
+
+                     out.writeUTF(help + "\n");
+
+                  } else if (Objects.equals(line, "DOW")) {
+
+                     out.writeUTF(day + "\n");
+
+                  } else if (Objects.equals(line, "TIME")) {
+                     out.writeUTF(new SimpleDateFormat("HH:mm:ss").format(time) + "\n");
+
+                  } else if (Objects.equals(line, "DATE")) {
+
+                     out.writeUTF(java.time.LocalDate.now() + "\n");
+
+                  } else if (Objects.equals(line, "DATETIME")) {
+
+                     out.writeUTF(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(dateTime) + "\n");
+                     // out.writeUTF(new SimpleDateFormat("yyyy").format(dateTime) + "\n");
+
+                  } else if (Objects.equals(line, "HELLO")) {
+
+                     out.writeUTF("ERROR Already in session" + "\n");
+
+                  } else if (line.isEmpty()) {
+                     break;
+
+                  } else {
+                     if (!line.equals("BYE") && !line.equals("EXIT")) {
+                        out.writeUTF("Input Invalid - Closing connection. \n");
+                        socket.close();
+                     } else {
+
+                        break;
+                     }
+                  }
+
+               } catch (IOException e) {
+                  // e.printStackTrace();
+                  break;
+
+               } catch (Exception e) {
+                  System.out.println("Error:" + socket);
+               }
+            }
+            out.writeUTF("Thank you for connecting to " + socket.getLocalSocketAddress() + "\nGoodbye!");
+            socket.close();
+
+         } catch (
+
+         IOException e) {
+            // e.printStackTrace();
+            // break;
+         }
+
+      }
+   }
+}
